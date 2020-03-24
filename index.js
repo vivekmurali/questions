32a58d5d9aeb088f74769edd29b483f9408b230a
Vivek Murali <vivekmurali2k@gmail.com>
2020-03-24 17:00:06 +0530

    Added authentication

--- index.js
+++ index.js
@@ -5,57 +5,87 @@ const mongoose = require('mongoose');
 const questions = require('./models/questions');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 dotenv.config();
 
 let port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server listening on port" + port);
 });
 
 
 
 //GET METHOD FOR HOMEPAGE
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
 
-    questions.find({},(err,question)=>{
-        res.render('questions.ejs',{Questions:question})
+    questions.find({}, (err, question) => {
+        res.render('questions.ejs', {
+            Questions: question
+        })
     });
-    
+
 });
+
+//UPDATED GET METHOD FOR ANSWER
+app.route('/answer')
+    .get((req, res) => {
+        res.render('auth.ejs');
+    })
+    .post((req, res) => {
+        var pass = req.body.password;
+        if (pass == process.env.AUTH) {
+            questions.find({}, (err, question) => {
+                res.render('answers.ejs', {
+                    Questions: question
+                });
+            });
+        }
+    })
+
 
 //GET METHOD FOR ANSWER
-app.get('/answer',(req,res)=>{
+// app.get('/answer',(req,res)=>{
 
-    questions.find({},(err,question)=>{
-        res.render('answers.ejs',{Questions:question})
-    });
-    
-});
+//     questions.find({},(err,question)=>{
+//         res.render('answers.ejs',{Questions:question})
+//     });
+
+// });
 
 
 //DELETING A QUESTION
 app.route('/remove/:id')
-    .get((req,res)=>{
+    .get((req, res) => {
         const id = req.params.id;
-        questions.findByIdAndRemove(id, err =>{
-            if(err) res.send(500,err);
-            res.redirect('/answer')
+        questions.findByIdAndRemove(id, err => {
+            if (err) res.send(500, err);
+            questions.find({}, (err, question) => {
+                res.render('answers.ejs', {
+                    Questions: question
+                });
+            });
         });
     });
 
 
 //ANSWERING A QUESTION 
 app.route('/answer/:id')
-    .post((req,res)=>{
+    .post((req, res) => {
         const id = req.params.id;
         questions.findByIdAndUpdate(id, {
             answer: req.body.answertext
-        }, err=>{
-            if(err) return res.send(500,err);
-            res.redirect('/answer');
+        }, err => {
+            if (err) return res.send(500, err);
+            questions.find({}, (err, question) => {
+                res.render('answers.ejs', {
+                    Questions: question
+                });
+            });
         });
     });
 
@@ -65,13 +95,18 @@ app.route('/answer/:id')
 
 //BOILER PLATE
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(__dirname+'/public'))
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + '/public'))
 
 
 //DATABASE CONNECT
 mongoose.set("useFindAndModify", false);
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
     console.log("Db COnnected!");
 });
 
@@ -79,21 +114,17 @@ mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopol
 
 
 //POST METHOD
-app.post('/',async (req,res)=>{
+app.post('/', async (req, res) => {
     const Question = new questions({
         content: req.body.question,
         answer: ''
     });
 
-    try{
+    try {
         await Question.save();
         res.redirect('/');
-    }
-    catch(err){
+    } catch (err) {
         console.log('err');
         res.redirect('/');
     }
 });
-
-
-
